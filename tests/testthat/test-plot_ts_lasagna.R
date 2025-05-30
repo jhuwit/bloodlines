d9260f0ed3b123bf203f11c8fda23abea5646139
lilykoff <koffmanlily@gmail.com>
2025-05-30 15:24:27 -0400

    add ts lasagna

--- tests/testthat/test-plot_ts_lasagna.R
+++ tests/testthat/test-plot_ts_lasagna.R
@@ -1,10 +1,10 @@
-testthat::test_that("create lasagna", {
+testthat::test_that("create ts lastagna", {
 
   # Load example data
   data(sample_df)
 
   # Test with default parameters
-  p <- plot_lasagna(sample_df, facet_var = "cat_cpb", layer_var = "map_cat")
+  p <- plot_ts_lasagna(sample_df, facet_var = "cat_cpb", layer_var = "map_cat")
 
   # Check if the plot is a ggplot object
   expect_true(inherits(p, "gg"))
